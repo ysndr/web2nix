790e313f22985adf997c418571bbae0a40a245d3
Gregory C. Oakes <gregoryoakes@fastmail.com>
2019-12-29 22:59:08 -0800

    Added xdg .desktop support.

--- default.nix
+++ default.nix
@@ -14,8 +14,28 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-{ pkgs ? import <nixpkgs> {}, url, name ? url, ... }:
-  pkgs.writeScriptBin name ''
-    #!${pkgs.runtimeShell}
-    exec ${pkgs.chromium}/bin/chromium --app=${pkgs.lib.escapeShellArg url}
-  ''
+{ pkgs ? import <nixpkgs> {}
+, url
+, name ? url
+, desktopItem ? {}
+, ...
+}:
+  let
+    xdgDefault = {
+      name = name;
+      exec = "${base}/bin/${name}";
+      icon = builtins.fetchurl {url = "${url}/favicon.ico";};
+      startupNotify = "true";
+      desktopName = with pkgs.lib;
+        (toUpper (substring 0 1 name) + substring 1 (-1) name);
+    };
+    base = pkgs.writeScriptBin name ''
+      #!${pkgs.runtimeShell}
+      exec ${pkgs.chromium}/bin/chromium --app=${pkgs.lib.escapeShellArg url}
+    '';
+    thisDesktopItem = pkgs.makeDesktopItem (xdgDefault // desktopItem);
+  in
+  pkgs.buildEnv {
+    name = name;
+    paths = [ base thisDesktopItem ];
+  }
